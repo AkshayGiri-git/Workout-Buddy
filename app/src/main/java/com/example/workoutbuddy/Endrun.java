7cf88d8a725a18c62aa0af8152c5a7962874e23b
Akshay Giri <superakshaygiri@gmail.com>
2021-07-25 20:19:30 -0700

    first commit

---
+++ app/src/main/java/com/example/workoutbuddy/Endrun.java
@@ -0,0 +1,14 @@
+package com.example.workoutbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Endrun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endrun);
+    }
+}
